38518b2291809e85635ea79b494be59d6dd94a8e
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-10-10 18:06:21 +0700

    update footer

--- src/components/footer/FooterMenu.tsx
+++ src/components/footer/FooterMenu.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const FooterMenu = () =>{
 
-    const items = ["Contact Us","Discord","Medium","Reddit","Games","Privacy","Term"];
+    const items = ["Contact Us","Discord","Medium","Reddit","Games","Privacy","Term"]
     const listItems = items.map((number) =>
-        <li className="text-[#b4b4b5] pt-[10px]">{number}</li>
+        <li className="text-[#b4b4b5] pt-[10px] cursor-pointer">{number}</li>
     );
 
     return (
