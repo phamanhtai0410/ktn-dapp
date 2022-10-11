d15116ff52da1a7415f460ef9ae202e6c20577e9
tinanhtom <tinanhtom02@gmail.com>
2022-10-11 14:23:07 +0700

    update home page

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './index.scss'
 import head_homepage from "../../assets/homepage/head_homepage.jpg"
-import Button from "../button/button"
 const Homepage = () => {
     return (
         <div>
@@ -10,14 +9,20 @@ const Homepage = () => {
                     <div className="flex flex-col metaverse justify-center w-[80%]">
                         <a className="font-bold w-fit metaverse-text text-white tracking-widest">METAVERSE
                             <p className="metaverse-subtext font-light text-white tracking-widest uppercase text-center">Buy Arena Genesis NFTs now</p>
+                            <div className="flex justify-center mt-4 gap-4">
+                                <button className="home-btn px-5 py-2 rounded-lg font-bold">
+                                    BUY ARENA NFTs
+                                </button>
+                                <button className="home-btn px-5 py-2 rounded-lg font-bold">
+                                    PLAY TRAILER
+                                </button>
+                            </div>
                         </a>
 
                     </div>
                 </div>
                 <img className="w-full z-[-1]" src={head_homepage} />
-                <div className="flex">
-                    <Button color="black" text={"BUY ARENA NFTs"}/>
-                </div>
+
             </div>
         </div>
     )
