d59684f2bf821d4539923bf2b26e55292c1c897e
Tra <tradaik1806@gmail.com>
2022-10-12 09:57:26 +0700

    update padding top min, cart

--- src/components/Partials/SelectTokensSymbol.tsx
+++ src/components/Partials/SelectTokensSymbol.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import IcArrow from '../../assets/images/partials/arrow_bot.svg'
 const SelectTokensSymbol = () => {
   return (
-    <div className="relative w-[216px] px-8 py-4 rounded-[32px] flex flex-col border border-white border-opacity-[0.4]">
+    <div className="relative w-[216px] px-8 py-3 rounded-[32px] flex flex-col border border-white border-opacity-[0.4]">
       <select
         className="flex flex-row items-center cursor-pointer appearance-none focus:outline-none bg-transparent text-[#a2a09e]"
         defaultValue={'BNB'}
