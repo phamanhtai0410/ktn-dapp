813a967371ac36f3a835d92b26ca22f17f8919fd
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-10-15 10:06:12 +0700

    fix load page

--- src/components/homepage/SessionInfo.tsx
+++ src/components/homepage/SessionInfo.tsx
@@ -10,7 +10,7 @@ import pancake from "../../assets/images/homepage/pancake.png"
 const SessionInfo = () => {
 
     return (
-        <div className="container mx-auto mt-[12vw] flex flex-col items-center w-full">
+        <div className="container mx-auto mt-[18vh] flex flex-col items-center w-full">
             <div className="text-center uppercase">
                 <p className="text-[2.5vw] text-white font-blome">buy katana inu NFTS TOKENS</p>
             </div>
