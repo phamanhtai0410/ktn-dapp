f953c86a3c4c77c3ff5086ceb79afdb256d7ef42
tinanhtom <tinanhtom02@gmail.com>
2022-10-19 16:20:13 +0700

    update sessionCollection carousel

--- src/components/homepage/ListCollections.tsx
+++ src/components/homepage/ListCollections.tsx
@@ -1,45 +1,75 @@
 import { selectCollections } from "@/reducers/collectionsSlice";
 import React from "react";
 import { useSelector } from "react-redux";
+import SwiperCore, { Autoplay, EffectCoverflow, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
 
-const sumTotal = (arr:any) => arr.reduce((sum:number, { price }) => sum + price , 0)
+SwiperCore.use([EffectCoverflow, Pagination, Autoplay]);
 
-const ItemCollection = ({item}) => (
-    <div className=" border border-[#584733] bg-black rounded-[0.5vw]" >
-        <div className="relative flex flex-col">
-            <div className="absolute z-[1] bottom-[7.5vw] w-full text-center text-white text-[0.8vw]">
-                <p className="text-[1.5vw] mb-[0.5vw] font-blome">{item.name}</p>
-                <p className="font-jost">{item.description}</p>
-            </div>
-            <img className="h-full z-[0]" src={`${item.image}?w-500`} />
-            <div className={`rare_${item.collection_id} z-[1]`}>
-                <div className="borderrar flex items-end">
-                    {item.nfts[0]?.rarity === 1 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Uncommon</span>}
-                    {item.nfts[0]?.rarity === 2 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Rare</span>}
-                    {item.nfts[0]?.rarity === 3 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Mythical</span>}
-                    {item.nfts[0]?.rarity === 4 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Legendary</span>}
-                    {item.nfts[0]?.rarity === 5 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Immortal</span>}
-                    <span className="text-white w-full text-right text-base align-bottom font-jost font-medium">
-                        $ {sumTotal(item.nfts || [])}
-                    </span>
-                </div>
-            </div>
-            <a href={`/cart/${item.collection_id}`} className="addbtn px-[1.5vw] py-[0.6vw] rounded-[32px] font-bold cursor-pointer m-[1vw] font-jost_medium">
-                Add to Cart
-            </a>
+const sumTotal = (arr: any) => arr.reduce((sum: number, { price }) => sum + price, 0)
+
+const ItemCollection = ({ item }) => (
+  <div className=" border border-[#584733] bg-black rounded-[0.5vw]" >
+    <div className="relative flex flex-col">
+      <div className="absolute bottom-[30%]  z-[1] w-full text-center text-white">
+        <p className="text-[25px] mb-[0.5vw] font-blome">{item.name}</p>
+        <p className="font-jost text-[16px]">{item.description}</p>
+      </div>
+      <img className="h-full mt-4 z-[0]" src={`${item.image}?w-500`} />
+      <div className={`rare_${item.collection_id} z-[1]`}>
+        <div className="borderrar flex items-end">
+          {item.nfts[0]?.rarity === 1 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Uncommon</span>}
+          {item.nfts[0]?.rarity === 2 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Rare</span>}
+          {item.nfts[0]?.rarity === 3 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Mythical</span>}
+          {item.nfts[0]?.rarity === 4 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Legendary</span>}
+          {item.nfts[0]?.rarity === 5 && <span className="rate-text align-bottom text-base font-jost font-medium capitalize">Immortal</span>}
+          <span className="text-white w-full text-right text-base align-bottom font-jost font-medium">
+            $ {sumTotal(item.nfts || [])}
+          </span>
         </div>
+      </div>
+      <a href={`/cart/${item.collection_id}`} className="addbtn px-[1.5vw] py-[0.6vw] rounded-[32px] font-bold cursor-pointer m-[1vw] font-jost_medium">
+        Add to Cart
+      </a>
     </div>
+  </div>
 )
 
 export default function ListCollection() {
   const items = useSelector(selectCollections);
   return (
-    <div className="hidden lg:grid grid-cols-4 gap-[2.5vw] w-[70%] mt-[3vw]">
-      {items.map((item, index) => {
+    <div className="w-[70%] mt-[3vw]">
+      <Swiper
+      effect={"fade"}
+      grabCursor={true}
+      loop={items.length > 2 ? true : false}
+      spaceBetween={20}
+      pagination={true}
+      autoplay={{ delay: 2000 }}
+      scrollbar={{ draggable: true }}
+      breakpoints={{
+        780: {
+          slidesPerView: 2,
+        },
+        1280: {
+          slidesPerView: 3,
+        },
+        1580: {
+          slidesPerView: 4,
+        },
+      }}
+      className="mySwiper"
+    >
+      {items.map((val, index) => {
         return (
-          <ItemCollection  key={index} item={item} />
-        )
+          <SwiperSlide key={index} className="w-[25%]">
+              <ItemCollection item={val} />
+          </SwiperSlide>
+        );
       })}
+    </Swiper>
     </div>
   );
 }
