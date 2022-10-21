35a194df088741b82a825bb64866bc7d6fb9abec
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-10-21 15:41:37 +0700

    update signature

--- src/actions/nftActions.ts
+++ src/actions/nftActions.ts
@@ -56,6 +56,21 @@ export const createOrder = createAsyncThunk(
     }
 )
 
+export const sendTxPaymentOrder = createAsyncThunk(
+    'nfts/TxPayment',
+    async (params:any, { dispatch, getState ,rejectWithValue}) => {
+        try {
+            const response = await NFTService.paymentOrder(params)
+            return response.data
+        } catch (err) {
+            if (!err.error_code) {
+                throw err
+            }
+            return rejectWithValue(err)
+        }
+    }
+)
+
 export const mintNftWithBSC = createAsyncThunk(
     'nfts/mintNftWithBSC',
     async (params:any, { dispatch, getState ,rejectWithValue}) => {
@@ -78,34 +93,33 @@ export const mintNftWithBSC = createAsyncThunk(
 
                 const dataMint = data.cids.map( (cid, index) => {
                     return {
-                        cid: cid,
                         rarity: data.rarities[index],
+                        cid: cid,
                         nftType: data.types[index],
                     }
                 })
 
-                console.log("signature",{
-                    ...signature,
-                    r: web3.utils.toHex(signature.r),
-                    s: web3.utils.toHex(signature.s),
-                    deadline: data.deadline
-                });
+                console.log("dataMint",dataMint)
+
+                const {r,s ,v} = ethers.utils.splitSignature(signature)
                 
-                // console.log("0----------",web3.utils.toHex(signature.r));
+                // console.log("Mining... please wait", {
+                //     r,
+                //     s,
+                //     v,
+                //     deadline: data.deadline
+                // });
 
                 let nftTxn = await contractNFT.makeMintingAction(
                     dataMint,
                     data.discount,
                     {
-                        ...signature,
-                        r: web3.utils.toHex(signature.r),
-                        s: web3.utils.toHex(signature.s),
+                        v,
+                        r,
+                        s,
                         deadline: data.deadline
-                    }
-                    // , { value: ethers.utils.parseEther("0.01") }
-                    );
-        
-                console.log("Mining... please wait");
+                    });
+
                 console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
                 
                 return await nftTxn.wait();
@@ -153,17 +167,68 @@ export const approveMint = createAsyncThunk(
                 accountBalance = ethers.utils.formatEther(accountBalance);
 
                 if(accountBalance < amount){
-                    alert("You not enough money")
                     throw ("You not enough money")
                 }
 
+                console.log("approveMint... please wait");
                 let approveTxn = await contractApprove.approve(
                     ADDRESS_CREATOR,
                     web3.utils.toWei(amount.toString())
                 );
+    
+                console.log(`approveMint, see transaction: https://rinkeby.etherscan.io/tx/${approveTxn.hash}`);
+                return  await approveTxn.wait();
+    
+            }
+            
+            
+        } catch (err) {
+            return rejectWithValue(err)
+        }
+    }
+)
+
+export const transferWalletDev = createAsyncThunk(
+    'nfts/transferWalletDev',
+    async (params:any, { dispatch, getState ,rejectWithValue}) => {
+
+        const rootState = getState() as RootState;
+        const  { easyWeb3 ,address} = rootState.wallet;
+
+        const signer = easyWeb3.getSigner();
+        const { amount , address_of_counter} = params;
         
-                console.log("Mining... please wait");
-                console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${approveTxn.hash}`);
+        console.log("address_of_counter",address_of_counter)
+
+        try {
+
+            if(signer && ADDRESS_CREATOR  && amount && address_of_counter ){
+
+
+                const contractTransfer = new ethers.Contract(
+                    "0xD9FfF9Ca72e2F4C3e613c770528198AFf2C6AC4B",
+                    ABI_ERC20,
+                    signer,
+                )
+                
+                console.log("contractTransfer",contractTransfer)
+                console.log("address",address)
+
+                let accountBalance = await contractTransfer.balanceOf(address);
+                accountBalance = ethers.utils.formatEther(accountBalance);
+
+                console.log("accountBalance",accountBalance)
+                if(accountBalance < amount){
+                    throw ("You not enough money")
+                }
+
+                console.log("transfer... please wait");
+                let approveTxn = await contractTransfer.transfer(
+                    address_of_counter,
+                    web3.utils.toWei(amount.toString())
+                );
+    
+                console.log(`transfer, see transaction: https://rinkeby.etherscan.io/tx/${approveTxn.hash}`);
                 return  await approveTxn.wait();
     
             }
--- src/components/cart/BtnPay.tsx
+++ src/components/cart/BtnPay.tsx
@@ -7,9 +7,9 @@ import { CircularProgress } from '@mui/material'
 import { useAppDispatch } from '@/app/hooks';
 import { NFTModel } from '@/models/redux-models';
 
-import { approveMint, createMetaDataNFT, createOrder, mintNftWithBSC } from '@/actions/nftActions';
+import { approveMint, createMetaDataNFT, createOrder, mintNftWithBSC, sendTxPaymentOrder, transferWalletDev } from '@/actions/nftActions';
 import { selectCartItems, selectCode } from '@/reducers/cartSlice';
-import { selectChain, selectWalletAccount } from '@/reducers/walletSlice';
+import { selectChain, selectEasyWeb3, selectWalletAccount } from '@/reducers/walletSlice';
 
 const sumTotal = (arr:NFTModel[]) => arr.reduce((sum:number, { price }) => sum + price , 0)
 
@@ -19,7 +19,7 @@ const BtnPay = () => {
     const [step, setStep] = useState("");
 
     const accountAddress = useSelector(selectWalletAccount);
-    const chainId = useSelector(selectChain);
+    const easyWeb3 = useSelector(selectEasyWeb3);
 
     const listItems = useSelector(selectCartItems);
     const promotion_code = useSelector(selectCode);
@@ -73,7 +73,11 @@ const BtnPay = () => {
                     if(!mintRes || mintRes.meta.requestStatus === "rejected"){
                         throw (mintRes.payload.reason);
                     }
+                    alert("Successfully!")
                 }
+
+                setStep("");
+                setIsPending(false);
                
             } else {
                 console.log("Ethereum object does not exist");
@@ -88,13 +92,19 @@ const BtnPay = () => {
 
     const createOrderAndMint = async () => {
 
+        if (isPending) { return; }    
+        setIsPending(true);
+
         try {
 
             const { ethereum } = window;
 
             if (ethereum && accountAddress) {
 
+                let amount = sumTotal(listItems);
+
                 // STEP 1: create order NFT
+                setStep("Pending...");
                 const orderData = await dispatch(createOrder({
                     items: listItems.map(item => {
                         return{
@@ -108,32 +118,67 @@ const BtnPay = () => {
                     promotion_code: ""
                 }))
 
+                console.log("orderData",orderData)
+
                 if(orderData.payload.error_code){
-                    alert(orderData.payload.msg);
-                    return;
+                    throw (orderData.payload.msg);
                 }
 
-                if(orderData.payload.data){
-                    const mintRes = await dispatch(mintNftWithBSC({
-                        data:   orderData.payload.data,
-                        signature :orderData.payload.signature,
+                // STEP 2 : Transfer wallet address dev
+                if(orderData.payload?.order_id){
+                    setStep("Transfer...");
+                    const mintRes = await dispatch(transferWalletDev({
+                        address_of_counter:   orderData.payload?.address_of_counter,
+                        amount
                     }))
+
+                    if(!mintRes || mintRes.meta.requestStatus === "rejected"){
+                        throw (mintRes.payload.reason|| mintRes.payload.message);
+                    }
+
+                    // // STEP 3 : Send log payment
+                    if(mintRes && mintRes.meta.requestStatus === "fulfilled"){
+                        setStep("Transfer...");
+                        await dispatch(sendTxPaymentOrder({
+                            order_id :   orderData.payload?.order_id,
+                            tx_hash : mintRes.payload.transactionHash
+                        }))
+                        alert("Successfully!")
+                    }
                 }
+                setStep("");
+                setIsPending(false);
                
             } else {
                 console.log("Ethereum object does not exist");
             }
     
         } catch (err) {
-          console.log(err);
+            console.log(err);
+            alert(err);
+            setIsPending(false);
         }
     }
+
+    const checkChainNetwork = async () => {
+
+        const {chainId} = easyWeb3.walletInfo;
+        console.log("checkChainNetwork",chainId);
+
+        if(chainId === 97){
+            mintNftHandler();
+        }else if(chainId === 5){
+            createOrderAndMint()
+        }
+
+    }
+
 
     return (
        <>
          {isPending ? <Beforeunload onBeforeunload={(event) => event.preventDefault()} /> : ""}
         <button
-            onClick={e=>{chainId ===97 ? mintNftHandler() : createOrderAndMint()}}
+            onClick={e=>{checkChainNetwork()}}
             className={`button w-full font-medium text-white text-base p-3 flex items-center justify-center rounded-[32px] cursor-pointer`}>
             { isPending ? <CircularProgress color="info" size="1.2rem" /> :"Pay with USDT" }  
             { isPending ? <span className='ml-2'>{step}</span> :"" }  
