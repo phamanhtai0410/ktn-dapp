24d350b912c1ad655eb637715a6dffdc90c72f23
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-10-21 22:46:56 +0700

    fix load navlink

--- src/pages/PageLoading.tsx
+++ src/pages/PageLoading.tsx
@@ -1,10 +1,13 @@
-import Spinner from 'react-spinner-material';
+// import Spinner from 'react-spinner-material';
 import React from 'react';
+
+import Preloader from '@/assets/images/preloader.svg'
 
 const PageLoading: React.FC = (): JSX.Element => {
     return (
-        <div>
-          <Spinner radius={120} color={"#333"} stroke={2} visible={true} />
+        <div className='w-full h-screen  backdrop-opacity-75 bg-[#171D22] flex items-center justify-center'>
+          {/* <Spinner radius={120} color={"#333"} stroke={2} visible={true} /> */}
+         <img src={Preloader} />
         </div>
     );
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const HomeComponent = React.lazy(() => import('@/pages/home/index'));
-const CartComponent = React.lazy(() => import('@/pages/cart'));
-const MintComponent = React.lazy(() => import('@/pages/mint'));
+const YOUR_DELAY = 800;
+// const HomeComponent = React.lazy(() => import('@/pages/home/index'));
+// const CartComponent = React.lazy(() => import('@/pages/cart'));
+// const MintComponent = React.lazy(() => import('@/pages/mint'));
+
+const HomeComponent = React.lazy(async () => {
+  await new Promise(resolve => setTimeout(resolve, YOUR_DELAY));
+  return import('@/pages/home/index');
+});
+
+const CartComponent = React.lazy(async () => {
+  await new Promise(resolve => setTimeout(resolve, YOUR_DELAY));
+  return import('@/pages/cart');
+});
+
+const MintComponent = React.lazy(async () => {
+  await new Promise(resolve => setTimeout(resolve, YOUR_DELAY));
+  return import('@/pages/mint');
+});
 
 const routes = [
   {
