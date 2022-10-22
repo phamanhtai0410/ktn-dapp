2af65d7751ca7eb192058d37ff5e459ed5fceea4
tinanhtom <tinanhtom02@gmail.com>
2022-10-22 12:05:08 +0700

    update footer and scrollbar

--- src/components/footer/FooterMenu.tsx
+++ src/components/footer/FooterMenu.tsx
@@ -1,8 +1,17 @@
-const FooterMenu = () =>{
+const FooterMenu = () => {
 
-    const items = ["Contact Us","Discord","Medium","Reddit","ChainVision Games","Privacy","Terms Of Service","Cookies Policy"]
-    const listItems = items.map((number) =>
-        <li key={number} className="text-[#b4b4b5] pt-[10px] cursor-pointer">{number}</li>
+    const items = [
+        {text:"Contact Us",link:"https://katanainu.com/#contact"},
+        {text:"Discord",link:"https://discord.com/invite/sruA5AZPAS"},
+        {text:"Medium",link:"https://medium.com/@katanainu/katana-inu-a-great-play2earn-battle-royale-pc-game-with-nft-marketplace-on-layer-2-a56f9bbcce79"},
+        {text:"Reddit",link:"https://www.reddit.com/r/katanainu"},
+        {text:"ChainVision Games",link:"https://katanainu.com/"},
+        {text:"Privacy",link:"https://katanainu.com/Katanainuprivacy.pdf"},
+        {text:"Terms Of Service",link:"https://katanainu.com/term-service"},
+        {text:"Cookies Policy",link:"https://katanainu.com/cookies-policy"}
+    ]
+    const listItems = items.map((number,index) =>
+        <li key={index} className="text-[#b4b4b5] pt-[10px] cursor-pointer"><a href={number.link} target="_blank">{number.text}</a></li>
     );
 
     return (
