7363f6f274bea202aa0f93b1b7a3500880d8ae17
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-10-28 10:51:23 +0700

    fix build env

--- vite.config.ts
+++ vite.config.ts
@@ -1,74 +1,111 @@
-import { defineConfig } from 'vite'
+import { defineConfig ,loadEnv } from 'vite'
 // import vitApp from '@vitjs/vit'
 import { join } from 'path'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 import autoImport from 'unplugin-auto-import/vite'
-// import windiCSS from 'vite-plugin-windicss'
+import windiCSS from 'vite-plugin-windicss'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import nodePolyfills from 'rollup-plugin-polyfill-node'
+import GlobalPolyFill from "@esbuild-plugins/node-globals-polyfill";
 
 const production = process.env.NODE_ENV === 'production'
+
 const resolve = (dir: string) => join(__dirname, dir)
 
 // https://vitejs.dev/config/
-export default ({ mode }) => defineConfig({
-  define: {
-    "process.env.NODE_ENV": `"${mode}"`,
-  },
+export default defineConfig(({ command, mode }) => {
 
-  resolve: {
-    alias: {
-      'node:stream': 'stream-browserify',
-      '@': resolve('src'),
-    },
-  },
- 
-  server: {
-    host: "0.0.0.0",
-    port: 3000
-  },
+  const env = loadEnv(mode, process.cwd(), '')
 
-  plugins: [
-    react({
-      babel: {
-        parserOpts: {
-          plugins: ['decorators-legacy'],
+    return{
+     
+      define: {
+        __APP_ENV__: env.APP_ENV
+      },
+      resolve: {
+        alias: {
+          '@': resolve('src'),
         },
       },
-    }),
-    tsconfigPaths(),
-    autoImport({
-      imports: [
-        'react',
-        {
-          react: [
-            'createElement',
-            'cloneElement',
-            'createContext',
-            // 'useLayoutEffect',
-            // 'forwardRef',
-          ],
+    
+      server: {
+        host: "0.0.0.0",
+        port: 3000
+      },
+      build: {
+        target: 'es2015',
+        minify: 'terser',
+        terserOptions: {
+          compress: {
+            drop_console: true, // 所有console
+            // pure_funcs: ['console.log'], // 单独指定
+            drop_debugger: true,
+          },
         },
-      ],
-    }),
-    // vitApp({
-    //   routes,
-    //   dynamicImport: {
-    //     loading: './components/PageLoading',
-    //   },
-    //   exportStatic: {},
-    // }),
-    //windiCSS(),
-    visualizer(),
-    // !production &&
-    //   nodePolyfills({
-    //     include: [
-    //       'node_modules/**/*.js',
-    //       new RegExp('node_modules/.vite/.*js'),
-    //     ],
-    //   }),
-      
-  ]
+        rollupOptions: {
+          plugins: [nodePolyfills()],
+        },
+        commonjsOptions: {
+          transformMixedEsModules: true,
+        },
+        /* 如需分包时开启 */
+        /*
+        rollupOptions: {
+          output: {
+            // 方式-1:所有依赖都分包
+            // manualChunks(id) {
+            //   if (id.includes('node_modules'))
+            //     return id.toString().split('node_modules/')[1].split('/')[0].toString()
+            // },
+            // 方式-2:只对大的依赖分包
+            manualChunks: {
+              vant: ['vant'], // 要分包的依赖
+            },
+          },
+        }, */
+      },
+
+      plugins: [
+        react({
+          babel: {
+            parserOpts: {
+              plugins: ['decorators-legacy'],
+            },
+          },
+        }),
+        tsconfigPaths(),
+        autoImport({
+          imports: [
+            'react',
+            {
+              react: [
+                'createElement',
+                'cloneElement',
+                'createContext',
+                // 'useLayoutEffect',
+                // 'forwardRef',
+              ],
+            },
+          ],
+        }),
+        // vitApp({
+        //   routes,
+        //   dynamicImport: {
+        //     loading: './components/PageLoading',
+        //   },
+        //   exportStatic: {},
+        // }),
+        windiCSS(),
+        visualizer(),
+        !production && mode === "development" && nodePolyfills({
+            include: [
+              'node_modules/**/*.js',
+              new RegExp('node_modules/.vite/.*js'),
+            ],
+        }),
+          
+      ]
+    }
 
 })
