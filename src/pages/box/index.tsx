82bd4fde2e4c98a0d4806e47abbc3e07706b1f90
Tra <tradaik1806@gmail.com>
2022-11-16 14:53:48 +0700

    animate box

--- src/pages/box/index.tsx
+++ src/pages/box/index.tsx
@@ -2,8 +2,8 @@ import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import bg from '../../assets/images/mint/bg.png'
 import layer_circle_box from '../../assets/images/box/layer_circle_box.png'
-import box from '../../assets/images/box/Box.png' 
-import Light from '../../assets/images/box/Light.png'
+import box from '../../assets/images/box/Box.png'
+import light from '../../assets/images/box/Light.png'
 
 import { fetchListNFTs } from '@/actions/nftActions'
 import { useAppDispatch } from '@/app/hooks'
@@ -18,16 +18,14 @@ import SessionListBox from '@/components/box/SessionListBox'
 
 import './index.scss'
 
-
 const Box = () => {
-
   const { id } = useParams()
   const { t } = useTranslation()
   const dispatch = useAppDispatch()
 
   useEffect(() => {
     if (id) {
-      fetchCartItems(id);
+      fetchCartItems(id)
     }
   }, [])
 
@@ -40,47 +38,54 @@ const Box = () => {
 
   return (
     <section className="mint text-center whitespace-pre-line pb-12 bg-black">
-       
       <div className="box__layer1 relative w-full flex flex-col items-center min-h-[1254px]">
-
-          <div className="mt-36 sm:px-0 px-4 flex flex-col items-center justify-center z-[1]">
-            <span className="box__focus-in-expand-fwd font-jost font-extrabold italic text-5xl text-[#f8a511] tracking-widest">
-              NFT MINTING
-            </span>
-            <span className="box__text-focus-in animate-delay-400 mt-[40px] font-jost font-bold text-2xl text-white uppercase">
-              katana inu takeru
-            </span>
-            <span className="box__text-focus-in animate-delay-800 mt-[18px] font-jost font-medium text-lg text-[#f8a511]">
-              Public sale starting soon
-            </span>
-            <div className="box__slide-top animate-delay-1200 md:mx-auto w-full md:w-auto mt-[18px] font-jost font-medium px-12 pt-2.5 pb-1.5 flex items-center justify-center rounded-[50px]">
-              {<Countdown eventTime={1669789211} interval={0} />}
-            </div>
+        <div className="mt-36 sm:px-0 px-4 flex flex-col items-center justify-center z-[1]">
+          <span className="box__focus-in-expand-fwd font-jost font-extrabold italic text-5xl text-[#f8a511] tracking-widest">
+            NFT MINTING
+          </span>
+          <span className="box__text-focus-in animate-delay-400 mt-[40px] font-jost font-bold text-2xl text-white uppercase">
+            katana inu takeru
+          </span>
+          <span className="box__text-focus-in animate-delay-800 mt-[18px] font-jost font-medium text-lg text-[#f8a511]">
+            Public sale starting soon
+          </span>
+          <div className="box__slide-top animate-delay-1200 md:mx-auto w-full md:w-auto mt-[18px] font-jost font-medium px-12 pt-2.5 pb-1.5 flex items-center justify-center rounded-[50px]">
+            {<Countdown eventTime={1669789211} interval={0} />}
           </div>
+        </div>
 
-          <div className="mt-12 flex flex-col items-center justify-center object-contain max-w-xl relative bg-box-light">
-            <img
-              src={box}
-              alt="cart"
-              className="mint__bounce-in-top animate-delay-1200 object-cover object-center z-10"
-            />
-          </div>
+        {/* <div className="mt-12 flex flex-col items-center justify-center object-contain max-w-xl relative bg-box-light">
+          <img
+            src={box}
+            alt="cart"
+            className="box__bounce-in-top top-0 left-0  object-cover object-center z-10"
+          />
+        </div> */}
 
-          <div className="sm:px-0 px-4 flex flex-col items-center justify-center z-[1]">
+        <div className="xs:mt-24 w-full min-h-[300px] flex flex-col items-center justify-center object-contain max-w-xl relative">
+          <img
+            src={light}
+            alt="cart"
+            className="absolute z-10 mix-blend-screen"
+          />
+          <img
+            src={box}
+            alt="cart"
+            className="box__bounce-in-top absolute xs:mb-9 sm:mb-0 mb-12  object-cover object-center z-10"
+          />
+        </div>
+
+        <div className="sm:px-0 px-4 flex flex-col items-center justify-center z-[1]">
           <ItemDetailNFT />
 
-            <BtnConnectWithMint />
+          <BtnConnectWithMint />
 
-            <div className="mt-9 w-full">
-              <ProgressBar percent={70} />
-            </div>
+          <div className="mt-9 w-full">
+            <ProgressBar percent={70} />
           </div>
-         
-       
-
+        </div>
       </div>
-      <SessionListBox/>
-
+      <SessionListBox />
     </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
       },
     },
     screens: {
+      xs: '400px',
+
       sm: '640px',
       // => @media (min-width: 640px) { ... }
 
