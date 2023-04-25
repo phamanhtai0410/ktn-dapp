d24bcf4c3ef291c46e3c0dc040b91d6f148395fa
khanhnguyen1003 <50676880+khanhnguyen1003@users.noreply.github.com>
2023-04-25 22:37:31 +0700

    _add_model-viewer_detail_nft_

--- src/components/mint/ImageNFTDetail.tsx
+++ src/components/mint/ImageNFTDetail.tsx
@@ -14,22 +14,35 @@ const ImageNFTDetail = () => {
 
   const listItems = useSelector(selectCartItems)
   const [checkQr,setCheckQr] = useState(false)
- 
+  
   return (
     <>
+       
         <div className="relative group bg-[#0D0F14] w-[400px] h-[440px] md:border-[8px] md:border-[#242632] mr-[40px] rounded-[10px]">
             {
                 !checkQr ? 
                 <div className="w-full h-full flex justify-center items-center">
-                  <img
-                    src={listItems[0]?.image ? listItems[0]?.image : char}
-                    alt="cart"
-                    className="mint__bounce-in-top animate-delay-1200 object-cover object-center rounded-[10px] h-full"
-                  /> 
+                  {
+                    listItems[0]?.animation_model_url ?
+                      <model-viewer
+                        style={{width: "100%", height: "100%"}}
+                        alt="3D image" 
+                        src={listItems[0]?.animation_model_url} ar ar-modes="webxr scene-viewer quick-look" 
+                        seamless-poster shadow-intensity="1" camera-controls auto-rotate
+                      />
+                    
+                    : <img
+                        src={listItems[0]?.image ? listItems[0]?.image : char}
+                        alt="cart"
+                        className="mint__bounce-in-top animate-delay-1200 object-cover object-center rounded-[10px] h-full"
+                      /> 
+                  }
+                  
                 </div>
               : <QrCode data={listItems[0]} />
             }
             <NavQrCode checkQr={checkQr} setCheckQr={setCheckQr} />
+            
         </div>
 
     </>
