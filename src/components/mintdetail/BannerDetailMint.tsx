124858d705d68b6c7d9be509b89b961170e8a671
khanhnguyen1003 <50676880+khanhnguyen1003@users.noreply.github.com>
2023-05-08 11:38:21 +0700

    _add_display_banner_

--- src/components/mintdetail/BannerDetailMint.tsx
+++ src/components/mintdetail/BannerDetailMint.tsx
@@ -13,7 +13,7 @@ const BannerDetailMint = () => {
   return (
     <div className="relative w-full flex items-center justify-center bg-no-repeat overflow-hidden">
       <img src={imgBanner} alt="" className='w-full hidden md:flex object-cover lg:max-h-[420px] xl:max-h-[470px]'/>
-      <img src={imgBannerMobile} alt="" className='w-full h-full' />
+      <img src={imgBannerMobile} alt="" className='w-full h-full md:hidden flex' />
       <div>
         <div className="absolute right-[17%] md:right-[6%] top-0 md:top-[14%] h-full flex flex-row items-center justify-center space-x-80 2xl:space-x-[440px]">
           <h1 className="text-white text-[14px] md:text-2xl font-normal uppercase whitespace-pre-line">
