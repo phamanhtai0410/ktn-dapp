909a545eaf8438b5b1c2cbc5c900f416a20fa7c0
khanhnguyen1003 <50676880+khanhnguyen1003@users.noreply.github.com>
2023-05-08 14:40:49 +0700

    _modify_footer_reponsive_

--- src/components/mint/DetailNFTSale.tsx
+++ src/components/mint/DetailNFTSale.tsx
@@ -5,7 +5,7 @@ const DetailNFTSale = () => {
     return (
         <div className="flex flex-row justify-center md:justify-normal  gap-x-[50px] mt-[38px] pt-[32px] border-t-[0.5px] border-[#C7C7C7]">
             <div className="flex flex-col">
-                <div className="flex items-center uppercase justify-center w-full md:w-[214px] h-[41px] px-[22px] py-[7px] text-[#F9C306] text-[10px] md:text-[20px] font-extrabold bg-[#000000] rounded-[12px]">
+                <div className="flex items-center uppercase justify-center w-full md:w-[214px] h-[41px] px-[11px] md:px-[22px] py-[7px] text-[#F9C306] text-[10px] md:text-[20px] font-extrabold bg-[#000000] rounded-[12px]">
                     Whitelist Sale
                 </div>
                 <div className="mt-[13px] text-center md:text-left uppercase text-[10px] md:text-[20px]">
@@ -15,7 +15,7 @@ const DetailNFTSale = () => {
                 </div>
             </div>
             <div className="flex flex-col">
-                <div className="flex items-center uppercase justify-center w-full md:w-[214px] h-[41px] px-[22px] py-[7px] text-[#F9C306] text-[10px] md:text-[20px] font-extrabold bg-[#000000] rounded-[12px]">
+                <div className="flex items-center uppercase justify-center w-full md:w-[214px] h-[41px] px-[11px] md:px-[22px] py-[7px] text-[#F9C306] text-[10px] md:text-[20px] font-extrabold bg-[#000000] rounded-[12px]">
                     Public Sale
                 </div>
                 <div className="mt-[13px] text-center md:text-left uppercase text-[10px] md:text-[20px]">
