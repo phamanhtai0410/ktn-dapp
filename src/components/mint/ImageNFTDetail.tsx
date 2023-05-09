7dc7bfe9cdbb98cc38e7565d1ff6ab4609f7426e
khanhnguyen1003 <50676880+khanhnguyen1003@users.noreply.github.com>
2023-05-09 16:27:58 +0700

    _add_video_detail_

--- src/components/mint/ImageNFTDetail.tsx
+++ src/components/mint/ImageNFTDetail.tsx
@@ -12,44 +12,57 @@ const ImageNFTDetail = () => {
 
   const listItems = useSelector(selectCartItems)
   const [checkQr,setCheckQr] = useState(false)
-  
+
+  const _filename = listItems[0]?.image?.split('.').pop();
+
   return (
     <>
        
         <div className="relative group bg-[#0D0F14] w-[400px] h-[440px] md:border-[8px] md:border-[#242632] mr-[40px] rounded-[10px]">
             {
                 !checkQr ? 
-                <div className="w-full h-full flex justify-center items-center">
-                  {
-                    listItems[0]?.animation_model_url ?
-                    
-                      <model-viewer
-                        style={{width: "100%", height: "75%"}}
-                        alt="3D image" 
-                        src={listItems[0]?.animation_model_url} ar ar-modes="webxr scene-viewer quick-look" 
-                        seamless-poster shadow-intensity="1" camera-controls auto-rotate
-                      />
+                  <div className="w-full h-full flex justify-center items-center">
+                    {
+                      listItems[0]?.animation_model_url ?
                       
-                    : 
-                    
-                    (
-                      listItems[0]?.image  ?
-                      <div className='h-[75%]'>
-                        <img
-                          src={listItems[0]?.image}
-                          alt="cart"
-                          className="mint__bounce-in-top animate-delay-1200 object-cover object-center rounded-[10px] h-full"
+                        <model-viewer
+                          style={{width: "100%", height: "75%"}}
+                          alt="3D image" 
+                          src={listItems[0]?.animation_model_url} ar ar-modes="webxr scene-viewer quick-look" 
+                          seamless-poster shadow-intensity="1" camera-controls auto-rotate
                         />
-                      </div> : 
-                      <ImageLoading />
-                    )
-                   
-                  }
-                  
-                </div>
+                        
+                      : 
+                      
+                      (
+                        listItems[0]?.image ?
+                          <div className='h-[75%]'>
+                            {
+                              _filename === 'mp4' ?
+                                <video controls autoPlay className='h-full'>
+                                    <source src={listItems[0]?.image} type="video/mp4" className='h-full' />
+                                    Your browser does not support the video tag.
+                                </video> 
+                                :
+                                <img
+                                  src={listItems[0]?.image}
+                                  alt="cart"
+                                  className="mint__bounce-in-top animate-delay-1200 object-cover object-center rounded-[10px] h-full"
+                                />
+                            }
+                            
+                          </div> : 
+                          <ImageLoading />
+                      )
+                    
+                    }
+
+                  </div>
               : <QrCode data={listItems[0]} />
             }
             <NavQrCode checkQr={checkQr} setCheckQr={setCheckQr} />
+
+            
             
         </div>
 
