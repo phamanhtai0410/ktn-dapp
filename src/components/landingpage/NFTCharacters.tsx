133c9a6f15b123e15726417d489608fbc5431f09
khanhnguyen1003 <50676880+khanhnguyen1003@users.noreply.github.com>
2023-05-10 14:31:47 +0700

    _modify_coming_soon_

--- src/components/landingpage/NFTCharacters.tsx
+++ src/components/landingpage/NFTCharacters.tsx
@@ -18,11 +18,9 @@ function NFTCharacter() {
   }
   const handleClick = (item) => {
     if (item.code === 'weapon') {
-      // navigate('/nfts?category=weapon&page=1&page_size=8&chain=ETHEREUM')
-      navigate(`/nfts?category=character&page=1&page_size=8&chain=ETHEREUM`)
+      navigate('/nfts?category=weapon&page=1&page_size=8&chain=ETHEREUM')
     } else if (item.code === 'pack') {
-      // navigate(`/nfts?category=pack&page=1&page_size=8&chain=ETHEREUM`)
-      navigate(`/nfts?category=character&page=1&page_size=8&chain=ETHEREUM`)
+      navigate(`/nfts?category=pack&page=1&page_size=8&chain=ETHEREUM`)
     } else {
       navigate(`/nfts?category=character&page=1&page_size=8&chain=ETHEREUM`)
     }
@@ -52,8 +50,8 @@ function NFTCharacter() {
                   <img src={character} alt="choose character" />
                 </div>
                 <div className="mt-[21px] text-[32px] font-bold text-[#F9C306] uppercase">
-                  {/* {item.name} */}
-                  Characters
+                  {item.name}
+                  {/* Characters */}
                 </div>
                 <div className="w-full h-[1px] bg-[#232428] md:my-[27px] my-[8px]"></div>
                 <div className="w-full h-[1px] mt-[14px] bgline bg-red"></div>
