da4f63732c02f87ffaf1d85f8f28244ee3c2dde7
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2023-05-11 12:05:46 +0700

    update whitelist_price 2

--- src/components/mint/BtnMint.tsx
+++ src/components/mint/BtnMint.tsx
@@ -176,18 +176,16 @@ const BtnMint = () => {
                     throw (metaData.payload.msg || metaData.payload);
                 }
 
-     
-
                 //STEP 2: mint NFT
                 setStep("Mint...")
                 if(metaData.payload.data){
-                    const {is_whitelist_mint}= metaData.payload.data;
+                    const {is_whitelist_mint ,whitelist_price}= metaData.payload.data;
 
                     if(is_whitelist_mint){
                         let listWhiteItems =listItems.map(e=>{
                             return{
                                 ...e,
-                                price: e.whitelist_price || e.price,
+                                price: whitelist_price || e.price,
                             }
                         })
                         amount = ( refCode ? sumCartDiscountTotal(listWhiteItems) : sumCartTotal(listWhiteItems));
