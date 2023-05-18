d01e6fd46a5e3038a23f934f3ee167a1bcb47ccc
khanhnguyen1003 <50676880+khanhnguyen1003@users.noreply.github.com>
2023-05-18 14:41:04 +0700

    _fix_empty_value_

--- src/components/AffiliateComponent/Earnings.tsx
+++ src/components/AffiliateComponent/Earnings.tsx
@@ -16,18 +16,20 @@ const Earnings = ({referralData}) => {
                 <div className="bg-[#11151B] rounded-[11px] py-[38px] px-[31px] w-[55%]">
                     <div>
                         <p className="text-[#FFFFFF] font-semibold text-[31px]">Total Earned</p>
-                        <p className="mt-[42px] text-center text-[#F9C306] font-bold text-[37px]">{referralData?.total_earn} $KATA</p>
-                        <p className="text-[21px] mt-[16px] text-center text-[#FFFFFF]">$239,739</p>
+                        <p className="mt-[42px] text-center text-[#F9C306] font-bold text-[37px]">
+                            { referralData? referralData?.total_earn : '0'} $KATA
+                        </p>
+                        {/* <p className="text-[21px] mt-[16px] text-center text-[#FFFFFF]">$239,739</p> */}
                     </div>
 
-                    <div className="flex flex-row gap-x-[60px] text-[#F1F1F1]">
+                    <div className="flex flex-row gap-x-[60px] text-[#F1F1F1] mt-[16px]">
                         <div className="flex flex-col gap-y-[21px]">
                             <span className="text-[18px] font-semibold">Your Rank</span>
                             <span className="text-[24px] font-bold">{userRank && userRank > 0 ? userRank : '--'}</span>
                         </div>
                         <div className="flex flex-col gap-y-[21px]">
                             <span className="text-[18px] font-semibold">Your Points</span>
-                            <span className="text-[24px] font-bold">{referralData?.total_earn}th</span>
+                            <span className="text-[24px] font-bold">{referralData? referralData?.total_earn : '0'}th</span>
                         </div>
                         <div className="flex flex-col gap-y-[21px]">
                             <span className="text-[18px] font-semibold">Referral People</span>
